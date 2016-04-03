99a92cd7db0307285b57ec8592c0f564e12142c3
Brian <bbensch@gmail.com>
2016-04-03 14:43:39 -0700

    begin building out debates view

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,8 +1,8 @@
 class Debate < ActiveRecord::Base
     belongs_to :judge_left, class_name: "Judge", foreign_key: "judge_left_id"
     belongs_to :judge_right, class_name: "Judge", foreign_key: "judge_right_id"
-    belongs_to :affirmative_debater, class_name: "User", foreign_key: "affirmative_id"
-    belongs_to :negative_debater, class_name: "User", foreign_key: "negative_id"
+    belongs_to :affirmative_debater, class_name: "Debater", foreign_key: "affirmative_id"
+    belongs_to :negative_debater, class_name: "Debater", foreign_key: "negative_id"
     belongs_to :topic
     has_many :rounds
     has_one :verdict
