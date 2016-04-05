95e7e9e04acd80cd7cb2d368697c6f68425f3bbd
Brian <bbensch@gmail.com>
2016-04-04 18:02:40 -0700

    install CKeditor and configure AWS for rich text formatting of debate messages

---
+++ app/models/ckeditor/picture.rb
@@ -0,0 +1,22 @@
+class Ckeditor::Picture < Ckeditor::Asset
+  has_attached_file :data,
+          styles: { content: "800>", thumb: "120x80>" }, default_url: "/images/:style/missing.png",
+          :storage => :s3,
+          :s3_credentials => {
+              :bucket => ENV['S3_BUCKET_NAME'],
+              :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+              :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+              },
+           :bucket => 'citizen-debate',
+           :url => ':s3_domain_url',
+           :path => '/:class/:attachment/:id_partition/:style/:filename',
+           :s3_host_name => 's3-us-west-2.amazonaws.com'
+
+  validates_attachment_presence :data
+  validates_attachment_size :data, :less_than => 2.megabytes
+  validates_attachment_content_type :data, :content_type => /\Aimage/
+
+  def url_content
+    url(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :civility_votes
   resources :debate_votes
   resources :messages
