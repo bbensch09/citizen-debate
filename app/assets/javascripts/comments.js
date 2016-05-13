0a6b01e6de552b0852ff77f0c257a325a96675d7
Brian <bbensch@gmail.com>
2016-05-13 12:48:44 -0700

    live messages with typing status notifications looking good

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,4 +1,7 @@
 window.client = new Faye.Client('/faye');
+$local_status = "You are waiting for a reply.";
+console.log("Local status is set. User is awaiting a reply.");
+
 
 client.addExtension({
   outgoing: function(message, callback) {
@@ -24,20 +27,31 @@ jQuery(function() {
 
 // Detect and log to console when text box is clicked into
 console.log("listening for form to be selected");
-document.getElementById('message-text-area').onclick = function() {userTyping() };
+document.getElementById('message-text-area').focusin = function() {userTyping() };
+document.getElementById('message-text-area').focusout = function() {awaitingReply() };
 
 function userTyping() {
-  console.log("Opponent is typing...");
-
   status_publisher = client.publish('/comments', {
-  status: 'Opponent is typing...'
+  status: 'Your opponent is typing...'
   });
-
+  $local_status = 'You are typing...';
+  $('.current-status').html($local_status);
   status_publisher.callback(function() {
-  console.log("status_publisher callback received");
-  // $('#message-text-area').val('');
+  console.log("user has begun typing");
 });
+  status_publisher.errback(function() {
+    alert('There was an error while posting your comment.');
+});
+}
 
+function awaitingReply() {
+  status_publisher = client.publish('/comments', {
+  status: 'Awaiting a reply...'
+  });
+  $local_status = "You are waiting for a reply.";
+  status_publisher.callback(function() {
+  console.log("user has stopped typing");
+});
   status_publisher.errback(function() {
     alert('There was an error while posting your comment.');
 });
@@ -49,11 +63,11 @@ function userTyping() {
       var sound_notification = new Audio('https://s3-us-west-2.amazonaws.com/citizen-debate/gets-in-the-way.mp3')
       sound_notification.play();
       // console.log("The payload is: "+payload.message)
-      return $('#comments').find('.media-list').prepend(payload.message);
+      return $('#comments').find('.media-list').append(payload.message);
     };
 
     // If typing status is published display it here.
-    if (payload.status){
+    if (payload.status && $local_status == "You are waiting for a reply." ){
       return $('.current-status').html(payload.status);
     }
 
