46b71e8e6e1e16ae2958faf04bed4107dc35de3f
Brian <bbensch@gmail.com>
2016-06-04 16:29:25 -0400

    add charity page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,14 @@
 class WelcomeController < ApplicationController
+
   def index
       @debate = Debate.where("status = 'Completed' ").last
       @pending_debates = Debate.where("status = 'Pending'")
   end
 
   def early_adopter_program
+  end
+
+  def charity_debate
+        render :layout => 'charity'
   end
 end
